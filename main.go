3e558393c1a483a3bcb13b3d48d623bccf270f83
M. Austin Roberto E <0315108515@miutt.edu.mx>
2020-12-21 20:44:22 -0800

    Se mejora ordena codigo

--- main.go
+++ main.go
@@ -1,62 +1,77 @@
 package main
 
 import (
-    // "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 );
 
-type Todo struct {
-	UserID    int    `json:"userId"`
-	ID        int    `json:"id"`
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
-}
-
 func main() {
-	get();
+	getStatusFromPageOldSATByPedimento("9004168");
+	getStatusFromPageNewSATByNumberOfIntegration("58343656");
 	http.HandleFunc("/", manejador)
 	http.ListenAndServe(":8080", nil)
-	
 }
 
 func manejador(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"Hola, %s, ¡este es un servidor!", r.URL.Path)
 }
 
-func get() {
-    fmt.Println("1. Performing Http Get...");
-	resp, err := http.Get("https://aplicacionesc.mat.sat.gob.mx/SOIANET/oia_consultarapd_cep.aspx?&pa=1673&dn=9004168&s=0&ap=2019&pad=400&ad=TIJUANA,%20B.C");
-	
-    if err != nil {
+func getStatusFromPageOldSATByPedimento(numPedimento string){
+	fmt.Println("Obteniendo datos de la pagina de SOIANET...");
+	resp, err := http.Get("https://aplicacionesc.mat.sat.gob.mx/SOIANET/oia_consultarapd_cep.aspx?&pa=1673&dn="+numPedimento+"&s=0&ap=2019&pad=400&ad=TIJUANA,%20B.C");
+
+	if err != nil {
         log.Fatalln(err);
+	} else {
+		respApiString := convertRespFromHttResponseToString(resp);
+	
+		//El tag que coincide
+		tag := `<font face="Arial" color="threeddarkshadow" size="2">`
+		//Posicion del ultimo tag encontrado del html
+		lastTag := strings.LastIndex(respApiString, tag);
+		//Nuevo respApiString sin el ultimo tag (Ya que no se requiere como el ultimo)
+		respApiString = respApiString[0:lastTag];
+		//Nueva posicion del tag encontrado actualizado
+		lastTag = strings.LastIndex(respApiString, tag);
+		//Recorte del tag a lo ultimo que contenga el html
+		respApiString = respApiString[lastTag+len(tag):len(respApiString)];
+		//Obtiene la posicion del primer <
+		lastTag = strings.Index(respApiString, "<");
+		//Recorte de lo ultimo en respApiString hasta la posicion del primer < encontrado
+		estatus := respApiString[0:lastTag];
+		fmt.Println("Estatus %s", estatus);
 	}
+}
 
-    defer resp.Body.Close();
-    bodyBytes, _ := ioutil.ReadAll(resp.Body);
+func getStatusFromPageNewSATByNumberOfIntegration(numIntegration string) {
+	fmt.Println("Obteniendo datos de la pagina de DODA...");
+	resp, err := http.Get("https://pecem.mat.sat.gob.mx/app/qr/ce/faces/pages/mobile/validadorqr.jsf?D1=16&D2=1&D3="+numIntegration);
+	if err != nil {
+        log.Fatalln(err);
+	} else {
+		asterisks := "***";
+		respApiString := convertRespFromHttResponseToString(resp);
+		positionInit := strings.Index(respApiString, asterisks);
+		positionFinal := strings.LastIndex(respApiString, asterisks);
+		status := respApiString[positionInit+len(asterisks):positionFinal];
+		fmt.Println("Estatus %s", status);
+	}
+}
 
-    // Convert response body to string
-    bodyString := string(bodyBytes);
+func convertRespFromHttResponseToString(respApi *http.Response) string {
+	return convertRespApiToString(convertRespApiToBytes(respApi));
+	
+}
 
-	// remplazar el ultimo encontrado por un vacio para obtener al final el ultimo que obtendra la info necesaria
+func convertRespApiToBytes(respApi *http.Response) []uint8 {
+	defer respApi.Body.Close();
+	bodyBytes, _ := ioutil.ReadAll(respApi.Body);
+	return bodyBytes
+}
 
-	//El tag que coincide
-	tag := `<font face="Arial" color="threeddarkshadow" size="2">`
-	//Posicion del ultimo tag encontrado del html
-	lastTag := strings.LastIndex(bodyString, tag);
-	//Nuevo bodyString sin el ultimo tag (Ya que no se requiere como el ultimo)
-	bodyString = bodyString[0:lastTag];
-	//Nueva posicion del tag encontrado actualizado
-	lastTag = strings.LastIndex(bodyString, tag);
-	//Recorte del tag a lo ultimo que contenga el html
-	bodyString = bodyString[lastTag+len(tag):len(bodyString)];
-	//Obtiene la posicion del primer <
-	lastTag = strings.Index(bodyString, "<");
-	//Recorte de lo ultimo en bodyString hasta la posicion del primer < encontrado
-	estatus := bodyString[0:lastTag];
-
-	fmt.Printf("estatus %s\n", estatus);
+func convertRespApiToString(respApiUnit8 []uint8) string {
+	return string(respApiUnit8);
 }
